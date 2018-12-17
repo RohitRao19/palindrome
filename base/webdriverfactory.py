c327bc3a3196a70d5094feaf2bac6eb9c50766d4
RohitRao19 <rohitrao19@gmail.com>
2018-12-17 11:54:18 -0500

    chrome options

--- base/webdriverfactory.py
+++ base/webdriverfactory.py
@@ -17,6 +17,9 @@ class WebDriverFactory():
         baseURL = "http://qa-home-palindrome.s3-website.us-east-2.amazonaws.com/login.html"
         options = Options()
         options.headless = True
+        options.add_argument('--headless')
+        options.add_argument('--no-sandbox')
+        options.add_argument('--disable-dev-shm-usage')
         if self.browser == "iexplorer":
             # Set ie driver
             driver = webdriver.Ie()
